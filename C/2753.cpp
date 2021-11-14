05b190b5328de7d7240d6ab322e155c7d7abf736
은지 <89649741+EunByu1@users.noreply.github.com>
2021-11-14 13:39:32 +0900

    Update 2753.cpp

--- C/2753.cpp
+++ C/2753.cpp
@@ -14,10 +14,10 @@ int main()
 // [2] 조건 비교 및 출력
 	if (( year % 4 ) == 0) 
 	{
-    	if ((year % 100 ) != 0)
-			{ 
+		if ((year % 100 ) != 0)
+		{ 
         	printf("1");
-    		}
+    	}
 		
 		else if (( year % 400 ) == 0) 
 		{
@@ -26,8 +26,8 @@ int main()
     	
 		else 
 		{
-        	printf("0");
-    	}
+    		printf("0");
+		}
 	}		// if ( ( year % 4 ) == 0 ) 
 	else 
 	{
